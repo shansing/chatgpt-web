497a7b066281102d986a6c56f238464f98ff4182
Shansing <i@shansing.com>
2023-09-29 21:52:43 +0800

    style: 思考中显示下划线

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -96,7 +96,7 @@ async function onConversation() {
     +uuid,
     {
       dateTime: new Date().toLocaleString(),
-      text: '',
+      text: '_',
       loading: true,
       inversion: false,
       error: false,
